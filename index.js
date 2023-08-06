08786ca66e733b16fd45936e49e5e4dcdbce744e
aboura <bouchrabichou638@gmail.com>
2023-08-06 20:13:41 +0200

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution in this file!
+
+
+var customerName = 'bob';
+const leastFavoriteCustomer = 'boby';
+
+
+function upperCaseCustomerName() {
+  return customerName = customerName.toUpperCase();
+}
+
+function upperCaseCustomerName() {
+  if (customerName) {
+    customerName = customerName.toUpperCase();
+  } else {
+    console.log("customerName is not defined.");
+  }
+}
+
+
+
+function setBestCustomer() {
+  return bestCustomer = 'not bob';
+}
+
+
+function overwriteBestCustomer() {
+  return bestCustomer = 'maybe bob';
+}
+
+
+
+
+function changeLeastFavoriteCustomer(name) {
+  return leastFavoriteCustomer = name;
+}
